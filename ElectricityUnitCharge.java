936b73f4a56c7778445184523b1e09f67de1b4e4
Ragul kannan <ragulkannan2735@gmail.com>
2025-02-15 15:19:33 +0530

    sql

--- ElectricityUnitCharge.java
+++ ElectricityUnitCharge.java
@@ -8,25 +8,38 @@ public class ElectricityUnitCharge {
       if(currentUnit<=50){
         bill+=currentUnit*0.50;
       }
+    
 
     else{
       if(currentUnit>50 && currentUnit<=150){
         bill=50*0.50;
         bill+=(currentUnit-50)*0.75;
-        if(currentUnit>150 && currentUnit<=250){
+      }
+        else if(currentUnit>150 && currentUnit<=250){
+          bill=50*0.50;
+          bill+=100*0.75;
           bill+=(currentUnit-150)*1.20;
-          if(currentUnit>250){
+        }
+        else if(currentUnit>250){
+          bill=50*0.50;
+          bill+=100*0.75;
+          bill+=100*1.20;
+          bill+=(currentUnit-250)*1.50;
+        
+        }
+      
+          else if(currentUnit>250){
             bill+=(currentUnit-250)*1.50;
           }
         }
       }
+        System.out.print(((bill*20)/100)+bill);
+        scan.close();
+      }
     }
-  }
+  
 
-    System.out.print((double)(bill*20)/100));
-    scan.close();
+
     
-  }
-  
-  
-}
+
+
